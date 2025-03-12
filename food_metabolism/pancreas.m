a1e10b7e8e2d5da06fbbb56a69e70fe96fce27f8
Vivek Booshan <vivek.booshan@gmail.com>
2025-03-12 00:19:11 -0400

    create Pancreas class and refactor insulin function with ligand class functions

--- food_metabolism/pancreas.m
+++ food_metabolism/pancreas.m
@@ -7,45 +7,61 @@
 % TODO : somatostatin needs to be self regulating
 % TODO : redefine glucose to modulate insulin, glucagon, and somatostatin secretion
 
-function dydt = pancreas(t, y, p)
-    dydt = zeros(4, 1);
-    dinsulin = insulin(t, y, p);
-    dglucagon = glucagon(t, y, p);
-    dsomatostatin = somatostatin(t, y, p);
-    dglucose = glucose(t, y, p);
+classdef Pancreas
+    methods (Static)
 
-    dydt = dinsulin + dglucagon + dsomatostatin + dglucose;
+        function dydt = pancreas(t, y, p)
+            dydt = zeros(4, 1);
+            dinsulin = insulin(t, y, p);
+            dglucagon = glucagon(t, y, p);
+            dsomatostatin = somatostatin(t, y, p);
+            dglucose = glucose(t, y, p);
+
+            % dydt = dinsulin + dglucagon + dsomatostatin + dglucose;
+        end
+
+        function dinsulin = insulin(t, I, R, p)
+            arguments (Input)
+                t double % timestamp
+                I double % Free Blood Insulin concentration
+                R double % Free Insulin Receptor concentration
+                p struct % Insulin parameter struct
+            end
+            arguments (Output)
+                dinsulin double
+            end
+
+            Kd = Ligand.ligand_Kd(L, R, LR);
+            fo = Ligand.fractional_occupancy(L, Kd);
+            dinsulin = p.k_insulin_secretion_insulin * ligand.fractional_inhibitory_effect(fo, 15) * I;
+            % dinsulin = dinsulin + glucose % upregulate
+            % dinsulin = dinsulin + glucagon % inhibit
+            % dinsulin = dinsulin + somatostatin % inhibit
+            % dinsulin = dinsulin + proteins % upregulate
+            % dinsulin = dinsulin + fatty acids % upregulate ? 
+        end
+
+
+        function dydt = glucagon(t, y, p)
+            dglucagon = (...
+                + p.glucagon_blood_pancreas * y(2) * p.V_blood ...
+                - p.glucagon_pancreas_portal * y(2) * p.V_pancreas ...
+            ) / p.V_pancreas;
+        end
+
+        function dydt = somatostatin(t, y, p)
+            dsomatostatin = (...
+                + p.somatostatin_blood_pancreas * y(3) * p.V_blood ...
+                - p.somatostatin_pancreas_portal * y(3) * p.V_pancreas ...
+            ) / p.V_pancreas;
+        end
+
+        function dydt = glucose(t, y, p)
+            dglucose = (...
+                + p.glucose_blood_pancreas * y(4) * p.V_blood ...
+                - p.glucose_pancreas_portal * y(4) * p.V_pancreas ...
+            ) / p.V_pancreas;
+        end
+
+    end
 end
-
-function dydt = insulin(t, y, p)
-    dydt = zeros(4, 1);
-    dydt(1) = (...
-        + p.insulin_blood_pancreas * y(1) * p.V_blood ...
-        - p.insulin_pancreas_portal * y(1) * p.V_pancreas ...
-    ) / p.V_pancreas;
-end
-
-function dydt = glucagon(t, y, p)
-    dydt = zeros(4, 1);
-    dydt(2) = (...
-        + p.glucagon_blood_pancreas * y(2) * p.V_blood ...
-        - p.glucagon_pancreas_portal * y(2) * p.V_pancreas ...
-    ) / p.V_pancreas;
-end
-
-function dydt = somatostatin(t, y, p)
-    dydt = zeros(4, 1); 
-    dydt(3) = (...
-        + p.somatostatin_blood_pancreas * y(3) * p.V_blood ...
-        - p.somatostatin_pancreas_portal * y(3) * p.V_pancreas ...
-    ) / p.V_pancreas;
-end
-
-function dydt = glucose(t, y, p)
-    dydt = zeros(4, 1);
-    dydt(4) = (...
-        + p.glucose_blood_pancreas * y(4) * p.V_blood ...
-        - p.glucose_pancreas_portal * y(4) * p.V_pancreas ...
-    ) / p.V_pancreas;
-end
-
