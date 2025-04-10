9dae052714b33a9bddf7e80d222d2a82c3e2bd91
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-09 22:08:41 -0400

    move globals to inside function and change ge --> geq

--- food_metabolism/pancreas.py
+++ food_metabolism/pancreas.py
@@ -1,48 +1,73 @@
 # https://www.researchgate.net/figure/Concentration-response-of-insulin-secretion-A-Average-insulin-secretion-rate_fig3_336211327
-im1 = (.75 - .0) / (11 - 2.5)
-im2 = (.85 - .75) / (17 - 11)
-im3 = (1 - 0.85) / (30 - 17)
-ix1 = 2.5
-ix2 = -34
-ix3 = -170/3
-
-gm1 = (0.3 - 1) / (7.5 - 0)
-gm2 = (0.55 - 0.3) / (20 - 7.5)
-gm3 = (1.3 - 0.55) / (30 - 20)
-gx1 = 75/7
-gx2 = 7.5
-gx3 = 38/3
-
-sm1 =  1 / 30
+# https://www.sciencedirect.com/science/article/pii/S0168822713004221
 
 def pancreas(Gblood):
-# Gblood is mmol
+    insulin_m1 = (.75 - .0) / (11 - 2.5)
+    insulin_m2 = (.85 - .75) / (17 - 11)
+    insulin_m3 = (1 - 0.85) / (30 - 17)
+    insulin_x1 = 2.5
+    insulin_x2 = -34
+    insulin_x3 = -170/3
+
+    glucagon_m1 = (0.3 - 1) / (7.5 - 0)
+    glucagon_m2 = (0.55 - 0.3) / (20 - 7.5)
+    glucagon_m3 = (1.3 - 0.55) / (30 - 20)
+    glucagon_x1 = 75/7
+    glucagon_x2 = -7.5
+    glucagon_x3 = 38/3
+
+    somatostatin_m1 =  1 / 30
+    # Gblood is mmol
+    k_baseline_insulin_secretion_max = 1
+    k_baseline_glucagon_secretion_empty = 1
+    k_baseline_somatostatin_secretion_max = 1
 
     # INSULIN
     dinsulin = 0
-    if Gblood > 30:
-        dinsulin += k_baseline_max
-    elif Gblood > 17:
-        dinsulin += im3 * k_baseline_max * (Gblood - ix3)
-    elif Gblood > 11:
-        dinsulin += im2 * k_baseline_max * (Gblood - ix2)
-    elif Gblood > 2.5:
-        dinsulin += im1 * k_baseline_max * (Gblood - ix1)
+    if Gblood >= 30:
+        dinsulin +=  k_baseline_insulin_secretion_max
+    elif Gblood >= 17:
+        dinsulin += insulin_m3 * k_baseline_insulin_secretion_max * (Gblood - insulin_x3)
+    elif Gblood >= 11:
+        dinsulin += insulin_m2 * k_baseline_insulin_secretion_max * (Gblood - insulin_x2)
+    elif Gblood >= 2.5:
+        dinsulin += insulin_m1 * k_baseline_insulin_secretion_max * (Gblood - insulin_x1)
 
 
     # GLUCAGON
     dglucagon = 0
-    if Gblood > 30:
-        dglucagon += 1.3 * k_baseline_glucagon_secretion_max
-    elif Gblood > 20:
-        dglucagon += gm3 * k_baseline_glucagon_secretion_max * (Gblood - gx3)
-    elif Gblood > 7.5:
-        dglucagon += gm2 * k_baseline_glucagon_secretion_max * (Gblood - gx2)
-    elif Gblood > 0:
-        dglucagon += gm1 * k_baseline_glucagon_secretion_max * (Gblood - gx1)
+    if Gblood >= 30:
+        dglucagon += 1.3 * k_baseline_glucagon_secretion_empty
+    elif Gblood >= 20:
+        dglucagon += glucagon_m3 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x3)
+    elif Gblood >= 7.5:
+        dglucagon += glucagon_m2 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x2)
+    elif Gblood >= 0:
+        dglucagon += glucagon_m1 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x1)
 
-        
     # SOMATOSTATIN
-    dsomatostatin = sm1 * k_baseline_somatostatin_secretion_max * Gblood
+    dsomatostatin = somatostatin_m1 * k_baseline_somatostatin_secretion_max * Gblood
 
     return (dinsulin, dglucagon, dsomatostatin)
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    Gblood = np.linspace(0, 30, 31)
+    hormones = np.array([pancreas(glucose) for glucose in Gblood])
+
+    hormone_labels = ('Insulin', 'Glucagon', 'Somatostatin')
+    colors = ('blue', 'green', 'red')
+
+    plt.figure(figsize=(10, 6))
+    for i in range(hormones.shape[1]):
+       plt.plot(Gblood, hormones[:, i], label=hormone_labels[i], color=colors[i])
+    plt.grid(color='gray', linestyle='--', linewidth=0.5)
+
+    plt.xlabel('Blood Glucose Concentration (mM)')
+    plt.ylabel('Hormone Secretion (%)')
+    plt.title('Hormone Secretion in Response to Blood Glucose Levels')
+    plt.legend()
+    plt.show()
+
