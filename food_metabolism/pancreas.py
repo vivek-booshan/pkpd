cbbe663ca27968f20ca523196f9a3edff134f1a1
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-10 23:57:42 -0400

    add uv lock file dependencies and create ode func for pancreas

--- food_metabolism/pancreas.py
+++ food_metabolism/pancreas.py
@@ -1,7 +1,17 @@
 # https://www.researchgate.net/figure/Concentration-response-of-insulin-secretion-A-Average-insulin-secretion-rate_fig3_336211327
 # https://www.sciencedirect.com/science/article/pii/S0168822713004221
 
-def pancreas(Gblood):
+def pancreas_dummy_ode(t, y):
+    kcl = 0.15
+
+    Gblood = y[0]
+    dGblood = -kcl * y[0]
+    dinsulin, dglucagon, dsomat = pancreatic_secretion_response_to_Gblood(Gblood)
+    dGcl = +kcl * y[0]
+    dydt = [dGblood, dinsulin, dglucagon, dsomat, dGcl]
+    return dydt
+
+def pancreatic_secretion_response_to_Gblood(Gblood):
     insulin_m1 = (.75 - .0) / (11 - 2.5)
     insulin_m2 = (.85 - .75) / (17 - 11)
     insulin_m3 = (1 - 0.85) / (30 - 17)
@@ -12,8 +22,10 @@ def pancreas(Gblood):
     glucagon_m1 = (0.3 - 1) / (7.5 - 0)
     glucagon_m2 = (0.55 - 0.3) / (20 - 7.5)
     glucagon_m3 = (1.3 - 0.55) / (30 - 20)
+    __glucagon_m2 = (0 - 0.3) / (30 - 7.5)
     glucagon_x1 = 75/7
     glucagon_x2 = -7.5
+    __glucagon_x2 = 30
     glucagon_x3 = 38/3
 
     somatostatin_m1 =  1 / 30
@@ -36,26 +48,31 @@ def pancreas(Gblood):
 
     # GLUCAGON
     dglucagon = 0
-    if Gblood >= 30:
-        dglucagon += 1.3 * k_baseline_glucagon_secretion_empty
-    elif Gblood >= 20:
-        dglucagon += glucagon_m3 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x3)
-    elif Gblood >= 7.5:
-        dglucagon += glucagon_m2 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x2)
-    elif Gblood >= 0:
+    # if Gblood >= 30:
+    #     dglucagon += 1.3 * k_baseline_glucagon_secretion_empty
+    # elif Gblood >= 20:
+    #     dglucagon += glucagon_m3 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x3)
+    # elif Gblood >= 7.5:
+    #     dglucagon += __glucagon_m2 * k_baseline_glucagon_secretion_empty * (Gblood - __glucagon_x2)
+    # elif Gblood >= 0:
+    #     dglucagon += glucagon_m1 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x1)
+     
+    if Gblood <= 7.5:
         dglucagon += glucagon_m1 * k_baseline_glucagon_secretion_empty * (Gblood - glucagon_x1)
+    elif Gblood <= 30:
+        dglucagon += __glucagon_m2 * k_baseline_glucagon_secretion_empty * (Gblood - __glucagon_x2)
 
     # SOMATOSTATIN
     dsomatostatin = somatostatin_m1 * k_baseline_somatostatin_secretion_max * Gblood
 
     return (dinsulin, dglucagon, dsomatostatin)
 
-if __name__ == "__main__":
+def plot_hormone_secretion_from_G_conc():
     import matplotlib.pyplot as plt
     import numpy as np
 
     Gblood = np.linspace(0, 30, 31)
-    hormones = np.array([pancreas(glucose) for glucose in Gblood])
+    hormones = np.array([pancreatic_secretion_response_to_Gblood(glucose) for glucose in Gblood])
 
     hormone_labels = ('Insulin', 'Glucagon', 'Somatostatin')
     colors = ('blue', 'green', 'red')
@@ -71,3 +88,48 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
+def plot_ode_soln():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from scipy.integrate import solve_ivp
+
+    t_span = (0, 6)
+    y0_Gblood = 1
+    y0 = [y0_Gblood, 0, 0, 0, 0]
+    sol = solve_ivp(pancreas_dummy_ode, t_span=t_span, y0=y0)
+    names = ("Plasma Glucose", "Insulin", "Glucagon", "Somatostatin", "Cleared Glucose")
+    # mass_balance = 1 - (sol.y[0, :] + sol.y[4, :])
+    # plt.plot(mass_balance)
+    # plt.show()
+    # plt.figure(figsize=(10, 6))
+    # for i in range(len(y0)):
+    #     plt.plot(sol.t, sol.y[i, :], label=names[i])
+    # plt.legend()
+    # plt.show()
+
+    fig, (ax_main, ax_mass) = plt.subplots(
+        nrows=2, 
+        figsize=(10, 6), 
+        gridspec_kw={'height_ratios': [4, 1]}, 
+        sharex=True
+    )
+
+    for i in range(len(y0)):
+        ax_main.plot(sol.t, sol.y[i, :], label=names[i])
+    ax_main.legend()
+    ax_main.set_ylabel("Concentration (Temporary Placeholder)")
+    ax_main.set_title("Pancreatic Hormone Secretion Kinetics")
+
+    mass_balance = 1 - (sol.y[0, :] + sol.y[4, :])
+    ax_mass.plot(sol.t, mass_balance, color='gray')
+    ax_mass.set_ylabel("Mass\nBalance")
+    ax_mass.set_xlabel("Time")
+    ax_mass.set_ylim([-1e-12, 1e-12])
+    ax_mass.axhline(0, linestyle='--', color='black', linewidth=0.8)
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    # plot_hormone_secretion_from_G_conc()
+    # plot_ode_soln()
