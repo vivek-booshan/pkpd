7b1c3ea479a64eec7c00e6616f41360516a36c07
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-16 10:43:22 -0400

    add skeletal muscle parameters to parameters

---
+++ food_metabolism/parameters.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+@dataclass
+class Parameters:
+    k_insulin_plasma_muscle
+    k_insulin_muscle_plasma
+
+    k_FA_plasma_muscle
+    k_FA_muscle_plasma
+
+    k_G_plasma_muscle
+    k_G_muscle_plasma
+
+    k_AA_plasma_muscle
+    k_AA_muscle_plasma
+
+    k_L_plasma_muscle
+    k_L_muscle_plasma
+
+    V_plasma
+
+    k_G_to_G6P
+    k_G6P_to_G
+
+    k_Glc_to_G6P
+    k_G6P_to_Glc
+
+    k_P_to_G6P
+    k_G6P_to_P
+
+    kCL_insulin
+
+    k_P_to_ACoA
+    k_ACoA_to_P
+
+    NADH_ETC
+    FADH2_ETC
+
+    k_FA_to_ACoA
+    k_AA_to_ACoA
+    k_P_to_L
+    k_L_to_P
+
+    kCL_ATP
+    
+    
+
+    
+
+    
+    
