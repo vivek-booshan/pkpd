f7ab0da483c8aac16f9d6eed831c3d648f3b891a
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-23 12:32:09 -0400

    buncha stuff

---
+++ food_metabolism/giFunctions.py
@@ -0,0 +1,129 @@
+import numpy as np
+from parameters import *
+
+def giInit():
+    p = Parameters(
+        V=Volumes(
+            plasma=5.0,
+            fat=11.0,
+            gut=1.25,
+            liver=0.0,
+            muscle=0.0,
+            pancreas=0.0,
+            brain=0.0
+        ),
+        shared=SharedRates(
+            k_P_to_ACoA=1.0,               # pyruvate_to_acetylcoa
+            k_ACoA_to_P=0.0,               # unused
+            k_FA_to_ACoA=1 / 8,            # fattyacids_to_acetylcoa
+            k_AA_to_ACoA=1 / 4,            # aminoacids_to_acetylcoa
+            k_ACoA_to_FA=1.0,              # acetylcoa_to_fattyacids
+            k_G_to_G6P=1.0,                # glucose_to_g6p
+            k_G6P_to_G=0.1,                # g6p_to_glucose
+            k_P_to_G6P=0.1,                # pyruvate_to_g6p
+            k_G6P_to_P=1.0                 # g6p_to_pyruvate
+        ),
+        CL=ClearanceRates(
+            kCL_insulin=0,
+            kCL_ATP=0.0,
+            kCL_G_GI=0.05,
+            kCL_F_GI=0.05,
+            kCL_FA_GI=0.1
+        ),
+        F=FatParameters(
+            k_insulin_from_plasma=1.0,
+            k_insulin_to_plasma=0.1,
+            k_FA_from_plasma=2.0,
+            k_FA_to_plasma=0.2,
+            k_G_from_plasma=1.0,
+            k_G_to_plasma=0.1,
+            k_AA_from_plasma=1.0,
+            k_AA_to_plasma=0.1,
+            k_FA_to_TAG=1.0,               # fattyacids_to_triglycerides
+            k_TAG_to_FA=0.1                # triglycerides_to_fattyacids
+        ),
+        M=None,
+        GI=GIParameters(
+            kabs_G=0.1,
+            kabs_F=0.1,
+            kabs_FA=0.05,
+            k_diffusion_micelle_to_membrane=0.1,
+            k_Vmax_trans=5.0,
+            k_Vmax_reester=3.0,
+            k_Vmax_export=2.0,
+            Km_trans=10.0,
+            Km_reester=5.0,
+            Km_export=4.0,
+        )
+        # Pancreas is not being used, so not included
+    )
+    return p
+
+def glucose_two_compartment(t, y, params):
+    # TODO : need to change this with respect to a holistic y
+    # (ie: F_gut, F_blood = y[m], y[n])
+    G_gut, G_blood = y
+
+    V_gut = params.V.gut
+    V_blood = params.V.plasma  # Using plasma as "blood" pool
+
+    kabs = params.GI.kabs_G
+    kclear = params.CL.kCL_G_GI
+
+    dG_gut_dt = -(kabs * G_gut * V_gut) / V_gut
+    dG_blood_dt = ((kabs * G_gut * V_gut) - (kclear * G_blood * V_blood)) / V_blood
+
+    return np.array([dG_gut_dt, dG_blood_dt])
+
+def fructose_two_compartment(t, y, params):
+    # TODO : need to change this with respect to a holistic y
+    # (ie: F_gut, F_blood = y[m], y[n])
+    F_gut, F_blood = y
+
+    V_gut = params.V.gut
+    V_blood = params.V.plasma
+
+    kabs = params.GI.kabs_F
+    kclear = params.CL.kCL_F_GI
+
+    dF_gut_dt = -(kabs * F_gut * V_gut) / V_gut
+    dF_blood_dt = ((kabs * F_gut * V_gut) - (kclear * F_blood * V_blood)) / V_blood
+
+    return np.array([dF_gut_dt, dF_blood_dt])
+
+def fatty_acid_two_compartment(t, y, params):
+    FA_gut, FA_blood = y
+
+    V_gut = params.V.gut
+    V_blood = params.V.plasma
+
+    kabs = params.GI.kabs_FA
+    kclear = params.CL.kCL_FA_GI
+
+    dFA_gut_dt = -(kabs * FA_gut * V_gut) / V_gut
+    dFA_blood_dt = ((kabs * FA_gut * V_gut) - (kclear * FA_blood * V_blood)) / V_blood
+    return [dFA_gut_dt, dFA_blood_dt]
+
+def fatty_acid_full_model(t, y, params):
+    # TODO : need to change this with respect to a holistic y
+    # (ie: F_gut, F_blood = y[m], y[n])
+    FA_mic, FA_mem, FA_cyto, TG_cyto, FA_blood = y
+
+    GI = params.GI
+    CL = params.CL
+    V_blood = params.V.plasma
+
+    # Pull out parameters
+    J_diff = GI.k_diffusion_micelle_to_membrane * (FA_mic - FA_mem)
+    J_trans = (GI.k_Vmax_trans * FA_mem) / (GI.Km_trans + FA_mem + 1e-6)
+    J_reester = (GI.k_Vmax_reester * FA_cyto) / (GI.Km_reester + FA_cyto + 1e-6)
+    J_export = (GI.k_Vmax_export * TG_cyto) / (GI.Km_export + TG_cyto + 1e-6)
+    J_clear = CL.kCL_FA_GI * FA_blood
+
+    dFA_mic_dt = -J_diff
+    dFA_mem_dt = J_diff - J_trans
+    dFA_cyto_dt = J_trans - J_reester
+    dTG_cyto_dt = J_reester - J_export
+    dFA_blood_dt = (J_export - J_clear)
+
+    return np.array([dFA_mic_dt, dFA_mem_dt, dFA_cyto_dt, dTG_cyto_dt, dFA_blood_dt])
--- food_metabolism/muscleFunctions.py
+++ food_metabolism/muscleFunctions.py
@@ -1,31 +1,52 @@
 import numpy as np
 from parameters import *
-n = 20
 
-def skeletalmuscle(t,y,p):
+
+def skeletalmuscle(t, y, p, n):
+    """
+    - 0 glucose plasma
+    - 1 glucose muscle
+    - 2 insulin plasma
+    - 3 insulin muscle
+    - 4 fattyacids plasma
+    - 5 fattyacids muscle
+    - 6 aminoacids plasma
+    - 7 aminoacids muscle
+    - 8 g6p muscle
+    - 9 glycogen muscle
+    - 10 Pyruvate
+    - 11 acetylcoa muscle
+    - 12 NAD 
+    - 13 NADH
+    - 14 FAD
+    - 15 FADH2
+    - 16 ROS
+    - 17 ATP
+    - 18 lactate in plasma
+    - 19 lactate in muscle
+    """
     dydt = np.zeros(n)
 
-    dglucose = glucose(t,y,p)
-    dinsulin = insulin(t,y,p)
-    dfattyacids = fattyacids(t,y,p)
-    daminoacids = aminoacids(t,y,p)
-    dg6p = g6p(t,y,p)
-    dglycogen = glycogen(t,y,p)
-    dpyruvate = pyruvate(t,y,p)
-    dacetylcoa = acetylcoa(t,y,p)
-    dNAD = NAD(t,y,p)
-    dNADH = NADH(t,y,p)
-    dFAD = FAD(t,y,p)
-    dFADH2 = FADH2(t,y,p)
-    dROS = ROS(t,y,p)
-    dATP = ATP(t,y,p)
-    dlactate = lactate(t,y,p)
+    glucose(t,y,p, dydt)
+    insulin(t,y,p, dydt)
+    fattyacids(t,y,p, dydt)
+    aminoacids(t,y,p, dydt)
+    g6p(t,y,p, dydt)
+    glycogen(t,y,p, dydt)
+    pyruvate(t,y,p, dydt)
+    acetylcoa(t,y,p, dydt)
+    NAD(t,y,p, dydt)
+    NADH(t,y,p, dydt)
+    FAD(t,y,p, dydt)
+    FADH2(t,y,p, dydt)
+    ROS(t,y,p, dydt)
+    ATP(t,y,p, dydt)
+    lactate(t,y,p, dydt)
     
-    dydt = dglucose+dinsulin+dfattyacids+daminoacids+dg6p+dglycogen+dpyruvate+dacetylcoa+dNAD+dNADH+dFAD+dFADH2+dROS+dATP+dlactate
     return dydt
 
-def glucose(t, y, p):
-    dydt = np.zeros(n)
+def glucose(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[0] = (
         (-p.M.k_G_from_plasma * y[0] * p.V.plasma + p.M.k_G_to_plasma * y[1] * p.V.muscle) / p.V.plasma
     )
@@ -33,10 +54,10 @@ def glucose(t, y, p):
         (p.M.k_G_from_plasma * y[0] * p.V.plasma - p.M.k_G_to_plasma * y[1] * p.V.muscle) / p.V.muscle
         - p.M.k_Glc_to_G6P * y[1] + p.M.k_G6P_to_Glc * y[8]
     )
-    return dydt
+    # return dydt
 
-def insulin(t, y, p):
-    dydt = np.zeros(n)
+def insulin(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[2] = (
         (-p.M.k_insulin_from_plasma * y[2] * p.V.plasma + p.M.k_insulin_to_plasma * y[3] * p.V.muscle) / p.V.plasma
     )
@@ -44,10 +65,10 @@ def insulin(t, y, p):
         (p.M.k_insulin_from_plasma * y[2] * p.V.plasma - p.M.k_insulin_to_plasma * y[3] * p.V.muscle) / p.V.muscle
         - p.CL.kCL_insulin * y[3]
     )
-    return dydt
+    # return dydt
 
-def fattyacids(t, y, p):
-    dydt = np.zeros(n)
+def fattyacids(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[4] = (
         (-p.M.k_FA_from_plasma * y[4] * p.V.plasma + p.M.k_FA_to_plasma * y[5] * p.V.muscle) / p.V.plasma
     )
@@ -55,10 +76,10 @@ def fattyacids(t, y, p):
         (p.M.k_FA_from_plasma * y[4] * p.V.plasma - p.M.k_FA_to_plasma * y[5] * p.V.muscle) / p.V.muscle
         - p.shared.k_FA_to_ACoA * y[5] * y[17]
     )
-    return dydt
+    # return dydt
 
-def aminoacids(t, y, p):
-    dydt = np.zeros(n)
+def aminoacids(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[6] = (
         (-p.M.k_AA_from_plasma * y[6] * p.V.plasma + p.M.k_AA_to_plasma * y[7] * p.V.muscle) / p.V.plasma
     )
@@ -66,10 +87,10 @@ def aminoacids(t, y, p):
         (p.M.k_AA_from_plasma * y[6] * p.V.plasma - p.M.k_AA_to_plasma * y[7] * p.V.muscle) / p.V.muscle
         - p.shared.k_AA_to_ACoA * y[7]
     )
-    return dydt
+    # return dydt
 
-def g6p(t, y, p):
-    dydt = np.zeros(n)
+def g6p(t, y, p, dydt):
+    # dydt = np.zeros(n)
     Km = 1
     dydt[8] = (
         + p.M.k_Glc_to_G6P * y[1]
@@ -79,10 +100,10 @@ def g6p(t, y, p):
         - p.shared.k_G6P_to_P * y[8] * y[12]**2
         + p.shared.k_P_to_G6P * y[10]**2 * y[17]**3 * y[13]**2
     )
-    return dydt
+    # return dydt
 
-def glycogen(t, y, p):
-    dydt = np.zeros(n)
+def glycogen(t, y, p, dydt):
+    # dydt = np.zeros(n)
     #adult weighing 70kg has about 400 g glycogenin muscle
     #1-2% muscle mass
     #20% of volume
@@ -92,10 +113,10 @@ def glycogen(t, y, p):
         p.shared.k_G6P_to_P * p.V.muscle * y[8] / (Km + y[8] * p.V.muscle)
         - p.shared.k_P_to_G6P * y[9]
     )
-    return dydt
+    # return dydt
 
-def pyruvate(t, y, p):
-    dydt = np.zeros(n)
+def pyruvate(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[10] = (
         2 * p.shared.k_G6P_to_P * y[8] * y[12]**2
         - 2 * p.shared.k_P_to_G6P * y[10]**2 * y[17]**3 * y[13]**2
@@ -103,65 +124,65 @@ def pyruvate(t, y, p):
         - p.M.k_P_to_L * y[10] * y[13]
         + p.M.k_L_to_P * y[19] * y[12]
     )
-    return dydt
+    # return dydt
 
-def acetylcoa(t, y, p):
-    dydt = np.zeros(n)
+def acetylcoa(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[11] = (
         p.shared.k_P_to_ACoA * y[10] * y[12]
         + 8 * p.shared.k_FA_to_ACoA * y[5] * y[17]
         + p.shared.k_AA_to_ACoA * y[7]
         - p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
     )
-    return dydt
+    # return dydt
 
-def NAD(t, y, p):
-    dydt = np.zeros(n)
+def NAD(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[12] = (
         -2 * p.shared.k_G6P_to_P * y[8] * y[12]**2
         + 2 * p.shared.k_P_to_G6P * y[10]**2 * y[17]**3 * y[13]**2
         - p.shared.k_P_to_ACoA * y[10] * y[12]
         - 3 * p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
         + 2 * p.M.NADH_ETC * y[13]**2
-        + p.shared.k_P_to_L * y[10] * y[13]
+        + p.M.k_P_to_L * y[10] * y[13]
         - p.M.k_L_to_P * y[19] * y[12]
     )
-    return dydt
+    # return dydt
 
-def NADH(t, y, p):
-    dydt = np.zeros(n)
+def NADH(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[13] = (
         2 * p.shared.k_G6P_to_P * y[8] * y[12]**2
         - 2 * p.shared.k_P_to_G6P * y[10]**2 * y[17]**3 * y[13]**2
         + p.shared.k_P_to_ACoA * y[10] * y[12]
         + 3 * p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
         - 2 * p.M.NADH_ETC * y[13]**2
-        - p.shared.k_P_to_L * y[10] * y[13]
+        - p.M.k_P_to_L * y[10] * y[13]
         + p.M.k_L_to_P * y[19] * y[12]
     )
-    return dydt
+    # return dydt
 
-def FAD(t, y, p):
-    dydt = np.zeros(n)
+def FAD(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[14] = (
         -p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
         + 2 * p.M.FADH2_ETC * y[15]**2
     )
-    return dydt
+    # return dydt
 
-def FADH2(t, y, p):
-    dydt = np.zeros(n)
+def FADH2(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[15] = (
         p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
         - 2 * p.M.FADH2_ETC * y[15]**2
     )
-    return dydt
+    # return dydt
 
-def ROS(t, y, p):
+def ROS(t, y, p, dydt):
     # might include a randomness aspect here. not sure what the probability will be
     ROSpercent = 0.02
     # 1-2% of molecular oxygen is converted to superoxide owing to electron leak
-    dydt = np.zeros(n)
+    # dydt = np.zeros(n)
     dydt[16] = ROSpercent * (
         p.shared.k_ACoA_to_P * y[11] * y[12]**3 * y[14]
         + p.M.FADH2_ETC * y[15]**2
@@ -169,11 +190,11 @@ def ROS(t, y, p):
         + p.shared.k_FA_to_ACoA * y[5] * y[17]
         + p.shared.k_AA_to_ACoA * y[7]
     )
-    return dydt
+    # return dydt
 
 
-def ATP(t, y, p):
-    dydt = np.zeros(n)
+def ATP(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[17] = (
         -p.shared.k_G_to_G6P * y[2] * y[17]
         + p.shared.k_G6P_to_G * y[8]
@@ -183,20 +204,20 @@ def ATP(t, y, p):
         + 3 * p.M.FADH2_ETC * y[15]**2
         + 5 * p.M.NADH_ETC * y[13]**2
         - p.shared.k_FA_to_ACoA * y[5] * y[17]
-        - p.CL.CLATP * y[17]
+        - p.CL.kCL_ATP * y[17]
     )
-    return dydt
+    # return dydt
 
-def lactate(t, y, p):
-    dydt = np.zeros(n)
+def lactate(t, y, p, dydt):
+    # dydt = np.zeros(n)
     dydt[18] = (
-        (-p.shared.kCL_FA * y[18] * p.V_plasma + p.shared.kCL_FA * y[19] * p.V_skeletalmuscle) / p.V_plasma
+        (-p.CL.kCL_FA * y[18] * p.V.plasma + p.CL.kCL_FA * y[19] * p.V.muscle) / p.V.plasma
     )
     dydt[19] = (
-        (p.shared.kCL_FA * y[18] * p.V_plasma - p.shared.kCL_FA * y[19] * p.V_skeletalmuscle) / p.V_skeletalmuscle
-        + p.shared.k_P_to_L * y[10] * y[13] - p.M.k_L_to_P * y[19] * y[12]
+        (p.CL.kCL_FA * y[18] * p.V.plasma - p.CL.kCL_FA * y[19] * p.V.muscle) / p.V.muscle
+        + p.M.k_P_to_L * y[10] * y[13] - p.M.k_L_to_P * y[19] * y[12]
     )
-    return dydt
+    # return dydt
 
 def muscle_init():
     p = Parameters(
@@ -214,7 +235,7 @@ def muscle_init():
             k_ACoA_to_P=5,               # acetylcoa_to_TCA
             k_FA_to_ACoA=1/8,            # fattyacids_to_acetylcoa
             k_AA_to_ACoA=1/4,            # aminoacids_to_acetylcoa
-            k_AcoA_to_FA=0.0,            # not provided
+            k_ACoA_to_FA=0.0,            # not provided
             k_G_to_G6P=1,                # glucose_to_g6p
             k_G6P_to_G=0.1,              # g6p_to_glucose
             k_P_to_G6P=0.1,              # pyruvate_to_g6p
