37a46e1f0c8d20242fbe4d4e35e65d21540914be
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-26 16:11:31 -0400

    refactor equations to start merging with rest of organs

--- food_metabolism/giFunctions.py
+++ food_metabolism/giFunctions.py
@@ -30,18 +30,8 @@ def giInit():
             kCL_F_GI=0.05,
             kCL_FA_GI=0.1
         ),
-        F=FatParameters(
-            k_insulin_from_plasma=1.0,
-            k_insulin_to_plasma=0.1,
-            k_FA_from_plasma=2.0,
-            k_FA_to_plasma=0.2,
-            k_G_from_plasma=1.0,
-            k_G_to_plasma=0.1,
-            k_AA_from_plasma=1.0,
-            k_AA_to_plasma=0.1,
-            k_FA_to_TAG=1.0,               # fattyacids_to_triglycerides
-            k_TAG_to_FA=0.1                # triglycerides_to_fattyacids
-        ),
+        Subq=None,
+        Vsc=None,
         M=None,
         GI=GIParameters(
             kabs_G=0.1,
@@ -59,37 +49,31 @@ def giInit():
     )
     return p
 
-def glucose_two_compartment(t, y, params):
-    # TODO : need to change this with respect to a holistic y
-    # (ie: F_gut, F_blood = y[m], y[n])
-    G_gut, G_blood = y
-
+def GI(t, y, p, n):
+    dydt = np.zeros(n)
+    glucose(t, y, p, dydt)
+    fructose(t, y, p, dydt)
+    fatty_acid_full_model(t, y, p, dydt)
+    
+def glucose(t, y, p, dydt):
     V_gut = params.V.gut
     V_blood = params.V.plasma  # Using plasma as "blood" pool
 
     kabs = params.GI.kabs_G
     kclear = params.CL.kCL_G_GI
 
-    dG_gut_dt = -(kabs * G_gut * V_gut) / V_gut
-    dG_blood_dt = ((kabs * G_gut * V_gut) - (kclear * G_blood * V_blood)) / V_blood
+    dydt[Index.gut_glucose] = -(kabs * y[Index.gut_glucose] * V_gut) / V_gut
+    dydt[Index.plasma_glucose] = ((kabs * y[Index.gut_glucose] * V_gut) - (kclear * y[Index.plasma_glucose]* V_blood)) / V_blood
 
-    return np.array([dG_gut_dt, dG_blood_dt])
-
-def fructose_two_compartment(t, y, params):
-    # TODO : need to change this with respect to a holistic y
-    # (ie: F_gut, F_blood = y[m], y[n])
-    F_gut, F_blood = y
-
+def fructose_two_compartment(t, y, p, dydt):
     V_gut = params.V.gut
     V_blood = params.V.plasma
 
     kabs = params.GI.kabs_F
     kclear = params.CL.kCL_F_GI
 
-    dF_gut_dt = -(kabs * F_gut * V_gut) / V_gut
-    dF_blood_dt = ((kabs * F_gut * V_gut) - (kclear * F_blood * V_blood)) / V_blood
-
-    return np.array([dF_gut_dt, dF_blood_dt])
+    dydt[Index.gut_fructose] = -(kabs * y[Index.gut_fructose] * V_gut) / V_gut
+    dydt[Index.plasma_fructose] = ((kabs * y[Index.gut_fructose] * V_gut) - (kclear * y[Index.plasma_fructose] * V_blood)) / V_blood
 
 def fatty_acid_two_compartment(t, y, params):
     FA_gut, FA_blood = y
@@ -104,26 +88,20 @@ def fatty_acid_two_compartment(t, y, params):
     dFA_blood_dt = ((kabs * FA_gut * V_gut) - (kclear * FA_blood * V_blood)) / V_blood
     return [dFA_gut_dt, dFA_blood_dt]
 
-def fatty_acid_full_model(t, y, params):
-    # TODO : need to change this with respect to a holistic y
-    # (ie: F_gut, F_blood = y[m], y[n])
-    FA_mic, FA_mem, FA_cyto, TG_cyto, FA_blood = y
-
+def fatty_acid_full_model(t, y, p, dydt):
     GI = params.GI
     CL = params.CL
     V_blood = params.V.plasma
 
     # Pull out parameters
-    J_diff = GI.k_diffusion_micelle_to_membrane * (FA_mic - FA_mem)
-    J_trans = (GI.k_Vmax_trans * FA_mem) / (GI.Km_trans + FA_mem + 1e-6)
-    J_reester = (GI.k_Vmax_reester * FA_cyto) / (GI.Km_reester + FA_cyto + 1e-6)
-    J_export = (GI.k_Vmax_export * TG_cyto) / (GI.Km_export + TG_cyto + 1e-6)
-    J_clear = CL.kCL_FA_GI * FA_blood
+    J_diff = GI.k_diffusion_micelle_to_membrane * (y[Index.micellar_fattyacid] - y[Index.membrane_fattyacid])
+    J_trans = (GI.k_Vmax_trans * y[Index.membrane_fattyacid]) / (GI.Km_trans + y[Index.membrane_fattyacid] + 1e-6)
+    J_reester = (GI.k_Vmax_reester * y[Index.cytosol_fattyacid]) / (GI.Km_reester + y[Index.cytosol_fattyacid] + 1e-6)
+    J_export = (GI.k_Vmax_export * y[Index.cytosol_TAG]) / (GI.Km_export + y[Index.cytosol_TAG] + 1e-6)
+    J_clear = CL.kCL_FA_GI * y[Index.plasma_glucose]
 
-    dFA_mic_dt = -J_diff
-    dFA_mem_dt = J_diff - J_trans
-    dFA_cyto_dt = J_trans - J_reester
-    dTG_cyto_dt = J_reester - J_export
-    dFA_blood_dt = (J_export - J_clear)
-
-    return np.array([dFA_mic_dt, dFA_mem_dt, dFA_cyto_dt, dTG_cyto_dt, dFA_blood_dt])
+    dydt[Index.micellar_fattyacid] = -J_diff
+    dydt[Index.membrane_fattyacid] = J_diff - J_trans
+    dydt[Index.cytosol_fattyacid] = J_trans - J_reester
+    dydt[Index.cytosol_TAG] = J_reester - J_export
+    dydt[Index.plasma_fattyacid] = (J_export - J_clear)
