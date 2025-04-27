15f4b026fc686baa629de54f21e3a02ef24305d4
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-27 12:33:29 -0400

    add module level doc

---
+++ food_metabolism/organs/__init__.py
@@ -0,0 +1,15 @@
+"""
+The `organs` module contains biological models for various human tissues and systems.
+
+Modules included:
+- fat.py: Fat tissue modeling and behavior.
+- muscle.py: Skeletal and smooth muscle tissue functions.
+- liver.py: Liver organ functions and properties.
+- pancreas.py: Pancreatic tissue and insulin modeling.
+- gi.py: Gastrointestinal system modeling.
+- system.py: Integration of different organ systems.
+- parameters.py: Common parameters and constants.
+- index.py: Provides an indexing enum and helper functions for accessing arrays
+
+This package supports simulation and analysis of organ-level metabolic processes.
+"""
